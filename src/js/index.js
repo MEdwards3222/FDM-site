5e22fffc27abf4211b1bc40f21045cc3bb0202b4
MEdwards3222 <medwards3222@gmail.com>
2023-11-20 15:07:52 -0500

    Added html webpack plugin

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,10 @@
 // Import our custom CSS
 import '../scss/styles.scss'
+import '../css/styles.css'
+import '../index.html'
+
+
+
 
 // Import all of Bootstrap's JS
 import * as bootstrap from 'bootstrap'
--- webpack.config.js
+++ webpack.config.js
@@ -11,6 +11,7 @@ module.exports = {
     port: 8080,
     hot: true
   },
+  
   module: {
     rules: [
         {
