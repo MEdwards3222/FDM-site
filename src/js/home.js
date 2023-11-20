83a53e00a4e6f61a41c7114be5c80b623db35bb6
MEdwards3222 <medwards3222@gmail.com>
2023-11-20 17:26:32 -0500

    Navbar completed - tested home

---
+++ src/js/home.js
@@ -0,0 +1,9 @@
+function initHome() {
+ let hello = document.createElement("div");
+
+ hello.textContent = "Hello, this is the Home Page."
+
+ return hello;
+}
+
+export {initHome}
--- src/js/initWebpage.js
+++ src/js/initWebpage.js
@@ -1,27 +1,81 @@
+import {initHome} from "./home";
+
+
 function initializePage() {
     const content = document.getElementById("content");
 
     content.appendChild(buildNavbar());
-    content.appendChild(helloWorld());
+    content.appendChild(initPageContent());
 
     return content
 }
 
 function buildNavbar() {
     let navbar = document.createElement('nav');
+    let fluidContainer = document.createElement('div');
+    let bankLogo = document.createElement('a');
+    let navbarNav = document.createElement('ul');
+    let homeList = document.createElement('li')
+    let newAccountList = document.createElement('li');
+    let updateAccountList = document.createElement('li');
+    let homeLink = document.createElement('a');
+    let newAccountLink = document.createElement('a');
+    let updateAccountLink = document.createElement('a');
+
 
     navbar.setAttribute("id", "navbar");
-    navbar.classList.add("navbar", "navbar-dark", "bg-primary");
+    navbar.classList.add("navbar", "navbar-dark", "bg-dark");
+
+    fluidContainer.classList.add("container-fluid");
+
+    bankLogo.setAttribute("href", "#");
+    bankLogo.classList.add("navbar-brand");
+    bankLogo.textContent = "FDM Bank";
+
+    navbarNav.setAttribute('id', 'navbarNav');
+    navbarNav.classList.add("navbar-nav");
+
+    homeList.classList.add("nav-item");
+    newAccountList.classList.add("nav-item");
+    updateAccountList.classList.add("nav-item");
+
+    homeLink.setAttribute("href", "#");
+    homeLink.classList.add('nav-link', 'active');
+    homeLink.textContent = "Home"
+
+    newAccountLink.setAttribute("href", "#");
+    newAccountLink.classList.add('nav-link');
+    newAccountLink.textContent = "Add";
+
+    updateAccountLink.setAttribute("href", "#");
+    updateAccountLink.classList.add('nav-link');
+    updateAccountLink.textContent = "Update";
+
+    homeList.appendChild(homeLink)
+    newAccountList.appendChild(newAccountLink)
+    updateAccountList.appendChild(updateAccountLink)
+    
+
+    navbarNav.appendChild(homeList);
+    navbarNav.appendChild(newAccountList);
+    navbarNav.appendChild(updateAccountList);
+    fluidContainer.appendChild(bankLogo);
+    fluidContainer.appendChild(navbarNav)
 
 
+    navbar.appendChild(fluidContainer);
     return navbar
 }
 
-function helloWorld() {
-    let hello = document.createElement('p');
-    hello.textContent = "Hello World!";
+function initPageContent() {
+    let pageContent = document.createElement("div");
 
-    return hello
+    pageContent.setAttribute("id", "page-content");
+    pageContent.classList.add("container");
+
+    pageContent.appendChild(initHome());
+
+    return pageContent
 }
 
 export default initializePage;
